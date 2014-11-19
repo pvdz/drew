61695845fde13f13ceb87090a0cd952bd6201594
Peter van der Zee <github@qfox.nl>
2014-11-19 15:11:07 +0100

    Add jumps and keyword checks to constants

--- constants.js
+++ constants.js
@@ -18,4 +18,11 @@ var constants = module.exports = {
   ENDOFLINE: '(token(index+1).type === EOF || isNewline(1))',
   STARTOFFILE: '!index',
   ENDOFFILE: 'token(index-1).type === EOF',
+
+  // skip from current curly to after the next
+  JUMP_TO_RHC: '(token().rhc && skipTo(token().rhc.white))',
+  JUMP_TO_RHS: '(token().rhs && skipTo(token().rhs.white))',
+  JUMP_TO_RHP: '(token().rhp && skipTo(token().rhp.white))',
+
+  KEYWORD: 'token().isKeyword',
 };
